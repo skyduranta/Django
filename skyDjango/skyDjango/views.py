804492bc6f7519a5ac686acc2902862537615d55
skyduranta <akashkr2712@gmail.com>
2024-06-24 00:05:14 +0530

    Django setup

---
+++ skyDjango/skyDjango/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello world this is Django home page")
+    return render(request, 'index.html')
+def about(request):
+    return HttpResponse("Hello world this is Django about page")
+def contact(request):
+    return HttpResponse("Hello world this is Django contact page") 
