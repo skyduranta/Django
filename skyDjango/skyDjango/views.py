76d1bf58125b392febe78010e8b113c46dc992be
skyduranta <akashkr2712@gmail.com>
2024-06-27 21:14:42 +0530

    templates in django

--- skyDjango/skyDjango/views.py
+++ skyDjango/skyDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello world this is Django home page")
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 def about(request):
     return HttpResponse("Hello world this is Django about page")
 def contact(request):
