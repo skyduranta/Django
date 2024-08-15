70f85911cff8ba0d1726781af226d0a360805a60
skyduranta <akashkr2712@gmail.com>
2024-08-15 23:46:16 +0530

    fullstack Django project

--- skyDjango/skyDjango/settings.py
+++ skyDjango/skyDjango/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sky',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+TAILWIND_APP_NAME ='theme'
+INTERNAL_IPS = ['127.0.0.1'] 
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'skyDjango.urls'
@@ -119,7 +128,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- skyDjango/skyDjango/urls.py
+++ skyDjango/skyDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from django.conf import settings 
+from django.conf.urls.static import static 
 from . import views
 
 urlpatterns = [
@@ -23,4 +25,7 @@ urlpatterns = [
     path('', views.home, name ='home'),
     path('about/', views.about, name ='about'),
     path('contact/', views.contact, name ='contact'),
-]
+    path('sky/', include('sky.urls' )),
+
+    path("__reload__/", include("django_browser_reload.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
